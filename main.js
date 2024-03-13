7d39f60700d67f711199e877dd0259d67db7f5d0
Reenakhan143 <159185285+Reenakhan143@users.noreply.github.com>
2024-03-13 21:16:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//let personName: String = "Eric";
+//console.log(`hello ${personName}
+var student = {
+    fullname: "reena",
+    lastname: "khan",
+    age: 27,
+    ispassed: true,
+    attendence: ["saturday", "monday", "tuesday",],
+    feeschedule: {
+        amount: 1000,
+        month: "march",
+        year: 2024,
+    }
+};
+var student2;
+console.log(student);
+console.log(student.age);
+console.log(student.attendence[2]);
+console.log(student.feeschedule.amount);
+console.log("detailn'\n" + student.attendence + "\nthanks");
---
+++ main.ts
@@ -0,0 +1,54 @@
+//let personName: String = "Eric";
+//console.log(`hello ${personName}
+
+let student={
+    fullname: "reena",
+    lastname: "khan",
+    age: 27,
+    ispassed: true,
+    attendence:["saturday","monday","tuesday",],
+    feeschedule: {
+        amount: 1000,
+        month:  "march",
+        year:  2024,
+
+        
+    }
+}
+let student2:{
+    fullname: "areeba",
+    lasrname: "khan",
+    age: 23,
+    ispassed: true,
+    aattendence:["monday","tuesday","friday"]
+}
+
+ 
+
+console.log(student)
+console.log(student.age)
+console.log(student.attendence[2])
+console.log(student.feeschedule.amount)
+console.log("detailn'\n"+ student.attendence+ "\nthanks")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
